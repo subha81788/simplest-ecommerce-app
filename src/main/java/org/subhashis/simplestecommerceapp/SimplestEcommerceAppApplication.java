fb88ad52b45e8a4975bfaf5b106e0cee520a1848
subha81788 <subha81788@gmail.com>
2020-10-06 18:33:06 +0530

    Encrypted database plain text password in properties file using Jasypt

--- src/main/java/org/subhashis/simplestecommerceapp/SimplestEcommerceAppApplication.java
+++ src/main/java/org/subhashis/simplestecommerceapp/SimplestEcommerceAppApplication.java
@@ -1,13 +1,38 @@
 package org.subhashis.simplestecommerceapp;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class SimplestEcommerceAppApplication {
 
+	@Value("${jasypt.encryptor.password}")
+	private String jasyptEncryptorSecret;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SimplestEcommerceAppApplication.class, args);
+	}
+
+	@Bean(name = "encryptorBean")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(jasyptEncryptorSecret);
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
 	}
 
 }
